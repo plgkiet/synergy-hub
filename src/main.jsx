f7983c4b318fbdc67cdf1cf21e36ae22d9d862be
plgkiet <plgkiet.work@gmail.com>
2026-03-16 22:55:21 +0700

    add close notice

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { SnackbarProvider } from "notistack";
+import { SnackbarProvider, closeSnackbar } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,8 +14,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         vertical: "top",
         horizontal: "right",
       }}
+      action={(key) => (
+        <button className="snackbar-close" onClick={() => closeSnackbar(key)}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
+      )}
     >
       <App />
     </SnackbarProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
